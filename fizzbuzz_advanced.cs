ca778bd96d201e7d1a30b80bab630d2a8b1dff7f
hckmtrx <96651154+hckmtrx@users.noreply.github.com>
2022-12-01 12:36:44 +0100

    Add files via upload

---
+++ fizzbuzz_advanced.cs
@@ -0,0 +1,16 @@
+﻿const string ENTER_NUMBER = "Enter the {0} number: ";
+
+Console.Write(ENTER_NUMBER, "first");
+int firstNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write(ENTER_NUMBER, "second");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write(ENTER_NUMBER, "third");
+int thirdNumber = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= firstNumber; i++)
+{
+    Console.Write(i % secondNumber == 0 && i % thirdNumber == 0 ? "FizzBuzz" : i % secondNumber == 0 ? "Fizz" : i % thirdNumber == 0 ? "Buzz" : i.ToString());
+    Console.Write(" ");
+}
