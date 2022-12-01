1f1699ac571e0c7ca9235177512d5a5c15a2ae15
hckmtrx <96651154+hckmtrx@users.noreply.github.com>
2022-12-01 12:31:35 +0100

    Add files via upload

---
+++ finding_the_operator.cs
@@ -0,0 +1,35 @@
+﻿const string ENTER_OPERAND = "Enter the {0} operand: ";
+
+Console.Write(ENTER_OPERAND, "first");
+double firstOperand = double.Parse(Console.ReadLine()!);
+
+Console.Write(ENTER_OPERAND, "second");
+double secondOperand = double.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the result: ");
+double result = double.Parse(Console.ReadLine()!);
+
+string operand = "+";
+string firstSecond = firstOperand + " {0} " + secondOperand + " = " + result;
+string secondFirst = secondOperand + " {0} " + firstOperand + " = " + result;
+
+if (firstOperand + secondOperand == result) { Console.WriteLine(firstSecond, operand); }
+
+operand = "-";
+if (firstOperand - secondOperand == result) { Console.WriteLine(firstSecond, operand); }
+if (secondOperand - firstOperand == result) { Console.WriteLine(secondFirst, operand); }
+
+operand = "*";
+if (firstOperand * secondOperand == result) { Console.WriteLine(firstSecond, operand); }
+
+operand = "/";
+if (Math.Round(firstOperand / secondOperand, 2) == result) { Console.WriteLine(firstSecond, operand); }
+if (Math.Round(secondOperand / firstOperand, 2) == result) { Console.WriteLine(secondFirst, operand); }
+
+operand = "^";
+if (Math.Pow(firstOperand, secondOperand) == result) { Console.WriteLine(firstSecond, operand); }
+if (Math.Pow(secondOperand, firstOperand) == result && firstOperand != secondOperand) { Console.WriteLine(secondFirst, operand); }
+
+operand = "%";
+if (firstOperand % secondOperand == result) { Console.WriteLine(firstSecond, operand); }
+if (secondOperand % firstOperand == result) { Console.WriteLine(secondFirst, operand); }
