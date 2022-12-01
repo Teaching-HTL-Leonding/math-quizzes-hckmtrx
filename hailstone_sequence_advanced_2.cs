ff2045fd7275f4eb4aa86ec62005df66f5a4ad57
hckmtrx <96651154+hckmtrx@users.noreply.github.com>
2022-12-01 16:59:10 +0100

    Add files via upload

--- hailstone_sequence_advanced_2.cs
+++ hailstone_sequence_advanced_2.cs
@@ -1,20 +1,28 @@
-const string ENTER_NUMBER = "Enter the {0} number: ";
-
-Console.Write(ENTER_NUMBER, "first");
-int n = int.Parse(Console.ReadLine()!);
+int n = LetUserEnterNumber("first");
 int firstNumber = n;
 
-Console.Write(ENTER_NUMBER, "second");
-int o = int.Parse(Console.ReadLine()!);
+int o = LetUserEnterNumber("second");
 int secondNumber = o;
 
 int lengthFirstNumber = 1;
 int lengthSecondNumber = 1;
 while (n > 1 || o > 1)
 {
-    if (n > 1) { lengthFirstNumber++; n = n % 2 == 0 ? n / 2 : 3 * n + 1; }
-    if (o > 1) { lengthSecondNumber++; o = o % 2 == 0 ? o / 2 : 3 * o + 1; }
+    if (n > 1) { lengthFirstNumber++; n = GetNextNumberOfHailstoneSequence(n); }
+    if (o > 1) { lengthSecondNumber++; o = GetNextNumberOfHailstoneSequence(o); }
 }
 
+Console.WriteLine();
 if (lengthFirstNumber == lengthSecondNumber) { Console.WriteLine("The length of the Hailstone sequence of these two numbers is the same"); }
 else { Console.WriteLine("The number {0} has the longer Hailstone sequence", lengthFirstNumber > lengthSecondNumber ? firstNumber : secondNumber); }
+
+int LetUserEnterNumber(string nthNumber)
+{
+    Console.Write($"Enter the {nthNumber} number: ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+int GetNextNumberOfHailstoneSequence(int currentNumber)
+{
+    return currentNumber % 2 == 0 ? currentNumber / 2 : 3 * currentNumber + 1;
+}
