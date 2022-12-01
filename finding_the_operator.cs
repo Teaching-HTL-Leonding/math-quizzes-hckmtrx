bd2a65cf81c9a81cc2afb0024b6a33b9ca2b215e
hckmtrx <96651154+hckmtrx@users.noreply.github.com>
2022-12-01 16:44:45 +0100

    Add files via upload

--- finding_the_operator.cs
+++ finding_the_operator.cs
@@ -1,35 +1,49 @@
-﻿const string ENTER_OPERAND = "Enter the {0} operand: ";
+﻿#region Constants
+const int DECIMAL_PLACES = 2;
+const string FIRST = "first";
+const string SECOND = "second";
+#endregion
 
-Console.Write(ENTER_OPERAND, "first");
-double firstOperand = double.Parse(Console.ReadLine()!);
-
-Console.Write(ENTER_OPERAND, "second");
-double secondOperand = double.Parse(Console.ReadLine()!);
+double firstOperand = LetUserEnterOperand(FIRST);
+double secondOperand = LetUserEnterOperand(SECOND);
 
 Console.Write("Enter the result: ");
 double result = double.Parse(Console.ReadLine()!);
 
 string operand = "+";
-string firstSecond = firstOperand + " {0} " + secondOperand + " = " + result;
-string secondFirst = secondOperand + " {0} " + firstOperand + " = " + result;
-
-if (firstOperand + secondOperand == result) { Console.WriteLine(firstSecond, operand); }
+if (firstOperand + secondOperand == result) { PrintTerm(FIRST); }
 
 operand = "-";
-if (firstOperand - secondOperand == result) { Console.WriteLine(firstSecond, operand); }
-if (secondOperand - firstOperand == result) { Console.WriteLine(secondFirst, operand); }
+if (firstOperand - secondOperand == result) { PrintTerm(FIRST); }
+if (secondOperand - firstOperand == result) { PrintTerm(SECOND); }
 
 operand = "*";
-if (firstOperand * secondOperand == result) { Console.WriteLine(firstSecond, operand); }
+if (firstOperand * secondOperand == result) { PrintTerm(FIRST); }
 
 operand = "/";
-if (Math.Round(firstOperand / secondOperand, 2) == result) { Console.WriteLine(firstSecond, operand); }
-if (Math.Round(secondOperand / firstOperand, 2) == result) { Console.WriteLine(secondFirst, operand); }
+if (Math.Round(firstOperand / secondOperand, DECIMAL_PLACES) == result) { PrintTerm(FIRST); }
+if (Math.Round(secondOperand / firstOperand, DECIMAL_PLACES) == result) { PrintTerm(SECOND); }
 
 operand = "^";
-if (Math.Pow(firstOperand, secondOperand) == result) { Console.WriteLine(firstSecond, operand); }
-if (Math.Pow(secondOperand, firstOperand) == result && firstOperand != secondOperand) { Console.WriteLine(secondFirst, operand); }
+if (Math.Pow(firstOperand, secondOperand) == result) { PrintTerm(FIRST); }
+if (Math.Pow(secondOperand, firstOperand) == result && firstOperand != secondOperand) { PrintTerm(SECOND); }
 
 operand = "%";
-if (firstOperand % secondOperand == result) { Console.WriteLine(firstSecond, operand); }
-if (secondOperand % firstOperand == result) { Console.WriteLine(secondFirst, operand); }
+if (firstOperand % secondOperand == result) { PrintTerm(FIRST); }
+if (secondOperand % firstOperand == result) { PrintTerm(SECOND); }
+
+double LetUserEnterOperand(string nthOperand)
+{
+    Console.Write($"Enter the {nthOperand} operand: ");
+    return double.Parse(Console.ReadLine()!);
+}
+
+void PrintTerm(string firstOrSecond)
+{
+    Console.WriteLine();
+    bool first = firstOrSecond == "first";
+    Console.Write(first ? firstOperand : secondOperand);
+    Console.Write($" {operand} ");
+    Console.Write(first ? secondOperand : firstOperand);
+    Console.Write($" = {result}");
+}
