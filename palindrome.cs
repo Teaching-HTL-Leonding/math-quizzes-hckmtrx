0d64d3aa2b0f825d298bdc4990d5ba1757365109
hckmtrx <96651154+hckmtrx@users.noreply.github.com>
2022-12-01 12:44:19 +0100

    Add files via upload

---
+++ palindrome.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+int originalNumber = number;
+
+int numberLength = 1;
+while (number % 10 != number) { numberLength++; number /= 10; }
+
+int numberReversed = 0;
+number = originalNumber;
+for (int i = 1; i <= numberLength; i++)
+{
+    numberReversed += (number % 10) * (int)Math.Pow(10, numberLength - i);
+    number /= 10;
+}
+
+Console.Write("The number is");
+Console.Write(originalNumber == numberReversed ? "" : " not");
+Console.Write(" a palindrome");
